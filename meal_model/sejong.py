edfa499fa68bcb8866f5a5943284c49d01c92277
changhwan77 <ckdghks9741@gmail.com>
2023-03-23 21:33:45 +0900

    오픈채팅방 알림 공지사항 멘트 추가

--- meal_model/sejong.py
+++ meal_model/sejong.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from dateutil.relativedelta import relativedelta
 from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 import locale
+from constants import Constants
 
 locale.setlocale(locale.LC_TIME, 'ko_KR.utf-8')
 
@@ -43,7 +44,7 @@ class Sejong:
         db_dinner=menu_item.dinner
         dinner = ', '.join(db_dinner.split('\n'))
         
-        menu= f'😊 {date_of_selected_menu} 식단입니다.\n\n🍙아침🍙\n{breakfast}\n\n🍘점심🍘\n{lunch}\n\n🍱저녁🍱\n{dinner}'
+        menu= f'😊 {date_of_selected_menu} 식단입니다.\n\n🍙아침🍙\n{breakfast}\n\n🍘점심🍘\n{lunch}\n\n🍱저녁🍱\n{dinner}\n\n{Constants.sejong_weekday_restaurant_hours_text}\n\n{Constants.for_notification_text}'
         return menu
     
     def fetch_week_menu(self, db:Session, content:dict):
